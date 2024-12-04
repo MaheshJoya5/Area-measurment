ec496cc2d43c309379230780d5cc2431befb1b8e
Kushan12 <k@echoinnovateit.com>
2024-12-04 23:00:10 +0530

    Initial commit

---
+++ Area MesurmentTests/Area_MesurmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Area_MesurmentTests.swift
+//  Area MesurmentTests
+//
+//  Created by Mahesh Joya on 04/12/24.
+//
+
+import Testing
+@testable import Area_Mesurment
+
+struct Area_MesurmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
